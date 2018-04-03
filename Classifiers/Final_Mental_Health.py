fe46fa12837461fbd4056e264781c1179ec57f53
Vipul Lohani <32271331+atyachari@users.noreply.github.com>
2018-04-03 20:12:07 +0530

    Removed unnecessary lines

--- Classifiers/Final_Mental_Health.py
+++ Classifiers/Final_Mental_Health.py
@@ -1,16 +1,8 @@
-
-# coding: utf-8
-
-# In[101]:
-
-
+#Importing dependencies
 import pandas as pd
 import numpy as np
 
-
-# In[161]:
-
-
+#Input data
 #data = pd.read_csv('addiction1.csv')
 data = pd.read_csv('anxiety1.csv')
 #data2 = pd.read_csv('autism1.csv')
@@ -21,39 +13,18 @@ data6 = pd.read_csv('selfharm1.csv')
 data7 = pd.read_csv('suicide1.csv')
 data8 = pd.read_csv('askreddit1_1.csv')
 data9 = pd.read_csv('buildape1.csv')
-
-
-# In[166]:
-
-
 data=data.append(data9)
-
-
-# In[167]:
-
 
 X = data['text']
 y = data['label']
-X.head(5)
+
 from sklearn.cross_validation import train_test_split
 X.shape
 
-
-# In[168]:
-
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-
-# In[169]:
-
 
 from sklearn.pipeline import Pipeline
 from sklearn.naive_bayes import MultinomialNB
-
-
-# In[170]:
-
 
 from nltk.stem.snowball import SnowballStemmer
 from sklearn.feature_extraction.text import CountVectorizer
@@ -71,29 +42,10 @@ text_mnb_stemmed = Pipeline([('vect', stemmed_count_vect),('tfidf', TfidfTransfo
 
 text_mnb_stemmed = text_mnb_stemmed.fit(X_train, y_train)
 
-
-# In[171]:
-
-
 predicted_mnb_stemmed = text_mnb_stemmed.predict(X_test)
 
 np.mean(predicted_mnb_stemmed == y_test)
-
-
-# In[196]:
-
-
 check = ['I need a knife']
-
-
-# In[197]:
-
-
 pred2=text_mnb_stemmed.predict(check)
-
-
-# In[198]:
-
-
 pred2
 
